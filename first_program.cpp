14b194dfa1cdcf084928c3eedba7457eda3711d3
EDreyer7709 <edreyer7709@floridapoly.edu>
2025-08-20 18:43:32 +0000

    Created the first C++ program

---
+++ first_program.cpp
@@ -0,0 +1,23 @@
+/*
+* 08/20/2025
+* The first program C++ program
+*
+*
+*/
+
+#include <iostream>
+
+using namespace std;
+
+// Create a main function
+int main(void) {
+std::cout << "Hello COP3337!" << std::endl;
+
+// std - standard namespace
+// :: - scope resolution operator
+// cout - console output (object of an iosteam class)
+// << - stream insertion operator
+// endl - end of line
+
+    return 0;
+}
